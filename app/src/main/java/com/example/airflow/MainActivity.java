889d12c56ebee568b70be5f10a403055abe05306
mwama100 <mwambachimba03@gmail.com>
2024-10-11 19:25:21 +0200

    Initial commit

---
+++ app/src/main/java/com/example/airflow/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.airflow;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button startButton = findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Move to the next screen or start functionality
+                Intent intent = new Intent(MainActivity.this, SmokeSensor.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
